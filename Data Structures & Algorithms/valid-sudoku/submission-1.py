44cc166e478c66e0cda748c5cc8f8ab93c203b27
Yoofi Brown-Pobee <insightnetwork.15@gmail.com>
2026-09-22 11:57:34 +0000

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = defaultdict(set)
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                entry = board[row][col]
+                if entry == ".":
+                    continue
+                col_grid = col // 3
+                row_grid = row //3
+                
+                if entry in seen[f"r_{row}"] or entry in seen[f"c_{col}"] or entry in seen[f"{row_grid}_{col_grid}"]:
+                    return False
+                else:
+                    seen[f"r_{row}"].add(entry)
+                    seen[f"c_{col}"].add(entry)
+                    seen[f"{row_grid}_{col_grid}"].add(entry)
+
+        return True
